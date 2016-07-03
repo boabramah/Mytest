65e701cc76d620e500cfaa42ff3256b8ffad483b
boabramah <boabramah@gmail.com>
2016-07-03 20:09:31 +0000

    The first commit of the repo

---
+++ index.php
@@ -0,0 +1,15 @@
+<div id="next-step">
+	
+
+<div id="next-step-header" class="section-parallax text-center parallax-window" data-parallax="scroll" data-bleed="0" data-image-src="<?php echo BASE_URL;?>images/next-step2.jpg" data-natural-width="1400" data-natural-height="900">
+    <div class="inner-dark-container">
+    	<div class="container">
+    		<h1 class="">Next Step</h1>
+    		<p class="big-paragraph"><?php echo $this->content->getSummary();?></p>
+    	</div>
+    </div>
+</div>	
+
+	<?php echo $this->content->getBody();?>
+
+</div>
